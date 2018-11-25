4f66d7e90e75b18d179869255999c39b70b7a083
Andrey <no3war@yandex.ru>
2018-11-25 23:55:45 +0300

    Create MechaCat.ino

---
+++ Arduino/MechaCat/MechaCat.ino
@@ -0,0 +1,258 @@
+/* This is main arduino sketch for MechaCat
+ * 
+ * More on GitHub: https://github.com/ikwic/Q-MechaCat
+ */
+
+#define MAIN_SKETCH
+#include "D:\Users\ikwic\Documents\Arduino\OpenCat\Nybble\WriteInstinct\OpenCat.h"
+#include <I2Cdev.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+
+#define PACKET_SIZE 42
+#define OVERFLOW_THRESHOLD 128
+
+//#if OVERFLOW_THRESHOLD>1024-1024%PACKET_SIZE-1   // when using (1024-1024%PACKET_SIZE) as the overflow resetThreshold, the packet buffer may be broken
+// and the reading will be unpredictable. it should be replaced with previous reading to avoid jumping
+#define FIX_OVERFLOW
+//#endif
+#define HISTORY 2
+int8_t lag = 0;
+float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+float yprLag[HISTORY][2];
+
+MPU6050 mpu;
+#define OUTPUT_READABLE_YAWPITCHROLL
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[PACKET_SIZE]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+
+char token;
+#define CMD_LEN 10
+char *lastCmd = new char[CMD_LEN];
+char *newCmd = new char[CMD_LEN];
+byte newCmdIdx = 0;
+byte hold = 0;
+
+uint8_t timer = 0;
+
+byte firstWalkingJoint;
+byte jointIdx = 0;
+
+
+unsigned long usedTime = 0;
+
+void checkBodyMotion()  {
+  if (!dmpReady) return;
+  // wait for MPU interrupt or extra packet(s) available
+  //while (!mpuInterrupt && fifoCount < packetSize) ;
+  if (mpuInterrupt || fifoCount >= packetSize)
+  {
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+    //PTL(fifoCount);
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount > OVERFLOW_THRESHOLD) { //1024) {
+      // reset so we can continue cleanly
+      mpu.resetFIFO();
+      // otherwise, check for DMP data ready interrupt (this should happen frequently)
+
+      // -- RzLi --
+#ifdef FIX_OVERFLOW
+      PTLF("FIFO overflow! Using last reading!");
+      lag = (lag - 1 + HISTORY) % HISTORY;
+#endif
+      // --
+    }
+    else if (mpuIntStatus & 0x02) {
+      // wait for correct available data length, should be a VERY short wait
+      while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+      // read a packet from FIFO
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+      // display Euler angles in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+#ifdef MPU_YAW180
+      ypr[2] = -ypr[2];
+#else
+      ypr[1] = -ypr[1] ;
+#endif
+#endif
+      /*PT(ypr[1] * degPerRad);
+        PTF("\t");
+        PTL(ypr[2] * degPerRad);*/
+      // overflow is detected after the ypr is read. it's necessary to keep a lag recrod of previous reading.  -- RzLi --
+#ifdef FIX_OVERFLOW
+      for (byte g = 0; g < 2; g++) {
+        yprLag[lag][g] = ypr[g + 1] * degPerRad;
+        ypr[g + 1] = yprLag[(lag - 1 + HISTORY) % HISTORY][g] * radPerDeg;
+      }
+      lag = (lag + 1) % HISTORY;
+#endif
+      // --
+      //deal with accidents
+      if (fabs(ypr[1])*degPerRad > LARGE_PITCH) {
+        PT(ypr[1] * degPerRad);
+        PTF("\t");
+        PTL(ypr[2] * degPerRad);
+        if (!hold) {
+          token = 'k';
+          strcpy(newCmd, ypr[1]*degPerRad > LARGE_PITCH ? "lifted" : "dropped");
+          newCmdIdx = 1;
+        }
+        hold = 10;
+      }
+      // recover
+      else if (hold) {
+        if (hold == 10) {
+          token = 'k';
+          strcpy(newCmd, "balance");
+          newCmdIdx = 1;
+        }
+        hold --;
+        if (!hold) {
+          char temp[CMD_LEN];
+          strcpy(temp, newCmd);
+          strcpy(newCmd, lastCmd);
+          strcpy(lastCmd, temp);
+          newCmdIdx = 1;
+          meow();
+        }
+      }
+      //calculate deviation
+      for (byte i = 0; i < 2; i++) {
+        RollPitchDeviation[i] = ypr[2 - i] * degPerRad - motion.expectedRollPitch[i];
+        RollPitchDeviation[i] = sign(ypr[2 - i]) * max(fabs(RollPitchDeviation[i]) - levelTolerance[i], 0);//filter out small angles
+      }
+    }
+  }
+}
+
+void setup() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  //Wire.setClock(400000);
+  TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  Serial.begin(57600);
+  Serial.setTimeout(10);
+  while (!Serial);
+  // wait for ready
+  while (Serial.available() && Serial.read()); // empty buffer
+  delay(100);
+  PTLF("\n* Starting *");
+  do {
+    PTLF("Initializing I2C");
+    mpu.initialize();
+    PTLF("Connecting MPU6050...");
+    delay(500);
+  } while (!mpu.testConnection());
+  
+  // verify connection
+  PTLF("Testing connections...");
+  PTL(mpu.testConnection() ? F("MPU successful") : F("MPU failed"));
+  
+  // load and configure the DMP
+  do {
+    PTLF("Initializing DMP...");
+    devStatus = mpu.dmpInitialize();
+    // supply your own gyro offsets here, scaled for min sensitivity
+
+    for (byte i = 0; i < 4; i++) {
+      PT(EEPROMReadInt(MPUCALIB + 4 + i * 2));
+      PT(" ");
+    }
+    PTL();
+    mpu.setZAccelOffset(EEPROMReadInt(MPUCALIB + 4));
+    mpu.setXGyroOffset(EEPROMReadInt(MPUCALIB + 6));
+    mpu.setYGyroOffset(EEPROMReadInt(MPUCALIB + 8));
+    mpu.setZGyroOffset(EEPROMReadInt(MPUCALIB + 10));
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+      // turn on the DMP, now that it's ready
+      PTLF("Enabling DMP...");
+      mpu.setDMPEnabled(true);
+
+      // enable Arduino interrupt detection
+      PTLF("Enabling interrupt detection");
+      attachInterrupt(0, dmpDataReady, RISING);
+      mpuIntStatus = mpu.getIntStatus();
+
+      // set our DMP Ready flag so the main loop() function knows it's okay to use it
+      PTLF("DMP ready!");
+      dmpReady = true;
+
+      // get expected DMP packet size for later comparison
+      packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      PTLF("DMP failed (code ");
+      PT(devStatus);
+      PTLF(")");
+      PTL();
+    }
+  } while (devStatus);
+
+  // servo
+  { pwm.begin();
+
+    pwm.setPWMFreq(60 * PWM_FACTOR); // Analog servos run at ~60 Hz updates
+    delay(200);
+
+//    strcpy(lastCmd, "rest");
+//    motion.loadBySkillName("rest");
+    for (byte i = 0; i < DOF; i++) {
+      pulsePerDegree[i] = float(PWM_RANGE) / servoAngleRange(i);
+      servoCalibs[i] = servoCalib(i);
+      calibratedDuty0[i] =  SERVOMIN + PWM_RANGE / 2 + float(middleShift(i) + servoCalibs[i]) * pulsePerDegree[i]  * rotationDirection(i) ;
+      PTL(SERVOMIN + PWM_RANGE / 2 + float(middleShift(i) + servoCalibs[i]) * pulsePerDegree[i] * rotationDirection(i) );
+      calibratedPWM(i, motion.dutyAngles[i]);
+    }
+    randomSeed(analogRead(0));//use the fluctuation of voltage caused by servos as entropy pool
+    shutServos();
+    token = 'd';
+  }
+  delay(1000);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
