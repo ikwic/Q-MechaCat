6e55116f8fd8c72f4905ac3d15da2637155d0b69
Andrey S <no3war@yandex.ru>
2019-03-14 00:18:52 +0300

    Update Setting_Servo_PCA9685-UART.ino
    tunning in microseconds

--- testing Servo/Setting_Servo_PCA9685-UART/Setting_Servo_PCA9685-UART.ino
+++ testing Servo/Setting_Servo_PCA9685-UART/Setting_Servo_PCA9685-UART.ino
@@ -3,16 +3,30 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define SERVOMIN       500
-#define SERVOMAX       2500
+#define SERVOMIN       680
+#define SERVOMAX       2200
 #define DEFAULT_PULSE_WIDTH   1500
 #define FREQUENCY             240
 
-int pos;
-int pulse;
+byte servoNum;
+int width;
+
+void setServoPulse (byte n, double pulse) {
+  double pulseLength;
+  
+  pulseLength = 1000000;  // 1,000,000 us per second
+  pulseLength /= FREQUENCY;
+  Serial.print(pulseLength); Serial.println(" us per period");
+  pulseLength /= 4096;  // 12 bits of resolution
+  Serial.print(pulseLength); Serial.println(" us per bit");
+
+  pulse /= pulseLength;
+  Serial.print("Setting "); Serial.print(n); Serial.print(" servo to "); Serial.println(pulse);
+  pwm.setPWM(n, 0, pulse);
+}
 
 void setup() {
-  // veify connection
+  // verify connection
   Serial.begin(115200);
   Serial.setTimeout(2000);
   Serial.println("Connected");
@@ -22,18 +36,18 @@ void setup() {
   pwm.setPWMFreq(FREQUENCY);
   delay(200);
   
+  Serial.println("Setting all servos to default position");  
   for(uint8_t i=0;i<16;i++) {
-    //Serial.print("Servo #:"); Serial.print(i); Serial.print("\t");
-    pwm.setPWM(i, 0, DEFAULT_PULSE_WIDTH);
+    setServoPulse(i, DEFAULT_PULSE_WIDTH);
   }
 }
 
 void loop() {
-  Serial.print("Write Servo # and pulse width: ");
+  Serial.print("Write Servo # and pulse length in us: ");
   Serial.flush();   while(!Serial.available());  // empty buffer and wait for input
-  pos = Serial.parseInt();
-  pulse = Serial.parseInt();
-  Serial.print(pos); Serial.print("-->"); Serial.println(pulse);
-  pwm.setPWM(pos, 0, pulse);
+  servoNum = Serial.parseInt();
+  width = Serial.parseInt();
+  Serial.print(servoNum); Serial.print("-->"); Serial.println(width);
+  setServoPulse(servoNum, width);
   delay(1000);
 }
